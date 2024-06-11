e31de489f1a1478ba5c0c52a286e28d2ccc3a0b5
Naman Arora <naarora@msc-linux-sls-019.scss.tcd.ie>
2024-06-11 19:36:18 +0100

    change build tareget

--- my-heap/heap-with-syscalls/build.sh
+++ my-heap/heap-with-syscalls/build.sh
@@ -2,7 +2,7 @@
 
 export TOOL_DIR=/opt/riscv32/
 
-$TOOL_DIR/bin/riscv32-unknown-elf-g++ -march=rv32im -std=gnu++11 combined.cpp -O3 -ffreestanding -Wl,-Bstatic -o firmware.elf -Wextra -Wshadow -Wundef -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wredundant-decls -g -pedantic -ffreestanding -fpermissive
+$TOOL_DIR/bin/riscv32-unknown-elf-g++ -march=rv32im -std=gnu++11 stl-heap-benchmark.cpp -O3 -ffreestanding -Wl,-Bstatic -o firmware.elf -Wextra -Wshadow -Wundef -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wredundant-decls -g -pedantic -ffreestanding -fpermissive
 
 $TOOL_DIR/bin/riscv32-unknown-elf-objcopy -O binary firmware.elf firmware.bin
 $TOOL_DIR/bin/riscv32-unknown-elf-objdump -s -t -r -d -f --source firmware.elf > objdump.txt
