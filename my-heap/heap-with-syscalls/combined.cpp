0be3f75b922cd2b4a3561fc3de371e40e82e806e
Naman Arora <naarora@msc-linux-sls-019.scss.tcd.ie>
2024-06-17 16:45:25 +0100

    reduce heap sizes

--- my-heap/heap-with-syscalls/combined.cpp
+++ my-heap/heap-with-syscalls/combined.cpp
@@ -116,7 +116,7 @@ int main() {
 
     std::srand(static_cast<unsigned>(std::time(nullptr))); // Use the standard library's time function
 
-    std::vector<size_t> heap_sizes = {10, 50, 100, 500, 1000};     
+    std::vector<size_t> heap_sizes = {10, 20, 50, 100}; // 500, 1000};     
     std::vector<int> pop_rates = {1, 5, 10, 20, 50}; // Expanded pop rates in percentage
     int duration_seconds = 1; // Reduced duration for the benchmark
 
